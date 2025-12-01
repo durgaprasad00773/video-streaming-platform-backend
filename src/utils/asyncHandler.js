253097ab37093a94616aeae840a53e7f000d0c97
Your Name <youremail@example.com>
2025-12-01 16:25:55 +0530

    add user and vide model

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -2,24 +2,26 @@
 // const asyncHandler = (func) => { () => {}}
 // const asyncHandler = (func) =>  async () => {}
 
-const asyncHandler = (func) => async(req, res, next) => {
-    try {
-        await func(req, res, next)
-    } catch (error) {
-        res.status(error.code || 500).json({
-            success : false,
-            message : error.message
-        })
+// const asyncHandler = (func) => async(req, res, next) => {
+//     try {
+//         await func(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success : false,
+//             message : error.message
+//         })
+//     }
+// }
+
+
+
+
+const asyncHandler = (func) => {
+    return (req, res, next) => {
+        Promise.resolve(func(req, res, next))
+         .catch((err) => next(err))
     }
 }
 
 export {asyncHandler}
-
-
-// const asyncHandler = (func) => {
-//     return (req, res, next) => {
-//         Promise.resolve(func(req, res, next))
-//          .catch((err) => next(err))
-//     }
-// }
 
